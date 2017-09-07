e8969372e6a1afa42b1bfb9e1fac9d55ad0151e9
Chris Wolfe <chriswwolfe@gmail.com>
2017-09-07 08:13:49 -0500

    log the requests

--- ticktock.go
+++ ticktock.go
@@ -118,11 +118,18 @@ func status(w http.ResponseWriter, r *http.Request) {
 	w.Write(js)
 }
 
+func Log(handler http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		log.Printf("%s %s %s", r.RemoteAddr, r.Method, r.URL)
+		handler.ServeHTTP(w, r)
+	})
+}
+
 func main() {
 	metricsInit()
 	updaterInit()
 
 	http.Handle("/metrics", promhttp.Handler())
 	http.HandleFunc("/", status) // set router
-	http.ListenAndServe(":9090", nil)
+	http.ListenAndServe(":9090", Log(http.DefaultServeMux))
 }
